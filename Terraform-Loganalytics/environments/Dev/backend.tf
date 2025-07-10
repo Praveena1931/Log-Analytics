4f74f9a4420fa77794678887312f2da8658eb20e
Praveena <praveena.y@algorims.com>
2025-07-10 16:17:09 +0530

    Infracost Added

--- Terraform-Loganalytics/environments/Dev/backend.tf
+++ Terraform-Loganalytics/environments/Dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tfstate-rg"      
-    storage_account_name = "tfstatestorageacct"      
+    resource_group_name  = "tfstate-rg1"      
+    storage_account_name = "tfstatestorageacct11"      
     container_name       = "tfstate"                 
     key                  = "terraform.tfstate"  
   }
